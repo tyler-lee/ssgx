b9f833d8d13637e10eb72849e1b3153778bcce5f
Tyler-Lee <lihuorong@iie.ac.cn>
2018-04-24 21:00:59 +0800

    remove unneeded functions

--- App/App.cpp
+++ App/App.cpp
@@ -242,22 +242,10 @@ int SGX_CDECL main(int argc, char *argv[])
         return -1;
     }
 
-    /* Utilize edger8r attributes */
-    //edger8r_array_attributes();
-    //edger8r_pointer_attributes();
-    //edger8r_type_attributes();
-    //edger8r_function_attributes();
-
-    /* Utilize trusted libraries */
-    //ecall_libc_functions();
-    //ecall_libcxx_functions();
-    //ecall_thread_functions();
-
 	lhr_measurement();
 
     /* Destroy the enclave */
     sgx_destroy_enclave(global_eid);
-
 
     //printf("Enter a character before exit ...\n");
     //getchar();
